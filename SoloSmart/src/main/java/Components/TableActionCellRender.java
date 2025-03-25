6abbaeb1f28f59e9ea109e5035b84a2cd9e1fd20
draflame <phulam03@gmail.com>
2025-03-25 11:21:47 +0700

    them edit table

---
+++ SoloSmart/src/main/java/Components/TableActionCellRender.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Components;
+
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+
+public class TableActionCellRender extends DefaultTableCellRenderer {
+    private String loai= "";
+    @Override
+    public Component getTableCellRendererComponent(JTable jtable, Object o, boolean isSeleted, boolean bln1, int row, int column) {
+       Component com = super.getTableCellRendererComponent(jtable, o, isSeleted, bln1, row, column);
+        PanelAction action = new PanelAction();
+        if (isSeleted == false && row % 2 == 0) {
+            action.setBackground(Color.WHITE);
+        } else {
+            action.setBackground(com.getBackground());
+        }
+        return action;
+    }
+    public TableActionCellRender(){
+        
+    }
+    public TableActionCellRender(String a){
+        this.loai=a;
+    }
+    
+    
+}
